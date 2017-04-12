53995aa012ecc28a4ca7d592698b2b259d5bb830
0Gemini0 <tlpelsmaeker@gmail.com>
2017-04-12 15:15:54 +0200

    Start IBM 2

--- Project-1/globals.py
+++ Project-1/globals.py
@@ -3,4 +3,4 @@ TRAIN_DATA_FILENAME = "training_pairs"
 VAL_DATA_FILENAME = "validation_pairs"
 DICT_FILENAME = "training_empty_dictionary"
 EPOCHS = 10
-THRESHOLD = 0.001
+THRESHOLD = 500000
--- Project-1/ibm1.py
+++ Project-1/ibm1.py
@@ -6,16 +6,19 @@ class IBM1:
         trans = {}
         for key in transProbs:
             trans[key] = {}
+            vocabSize = len(transProbs[key].keys())
             for secKey in transProbs[key]:
-                trans[key][secKey] = random.uniform(0.00001, 0.9999999)
+                trans[key][secKey] = 1.0 / vocabSize
         self.transProbs = trans
+        
         
     def randomize(self, transProbs):
         trans = {}
         for key in transProbs:
             trans[key] = {}
+            vocabSize = len(transProbs[key].keys())
             for secKey in transProbs[key]:
-                trans[key][secKey] = random.uniform(0.00001, 0.9999999)
+                trans[key][secKey] = 1.0 / vocabSize
         self.transProbs = trans
         
 
@@ -43,21 +46,23 @@ class IBM1:
 
             # loop over sentences, french words and english words
             # Expectation - step
+            print "E"
             for pair in pairs:
-                logLike += np.log(1 / (len(pair[0]) + 1) ** len(pair[1]))
-                for i, fWord in enumerate(pair[1]):
+                logLike += -(len(pair[1]) * np.log(len(pair[0])+1))
+                for fWord in pair[1]:
                     # calculate the normalizer of the posterior probability of this french word
                     normalizer = 0
-                    for eWord in enumerate(pair[0]):
+                    for eWord in pair[0]:
                         normalizer += transProbs[eWord][fWord]
 
                     logLike += np.log(normalizer)
                     # get the expected counts based on the posterior probabilities
-                    for j, eWord in enumerate(pair[0]):
+                    for eWord in pair[0]:
                         counts[eWord][fWord] += (transProbs[eWord][fWord] / normalizer)
                         countsEnglish[eWord] += (transProbs[eWord][fWord] / normalizer)
 
             logLikelihood.append(logLike)
+            print logLike
 
             # check for log-likelihood convergence
             if len(logLikelihood) > 1:
@@ -67,6 +72,7 @@ class IBM1:
                     break
 
             # Maximization - step
+            print "M"
             for eKey in transProbs:
                 for fKey in transProbs[eKey]:
                     transProbs[eKey][fKey] = counts[eKey][fKey] / countsEnglish[eKey]
