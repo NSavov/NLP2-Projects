591134d7ab48719233f979b6806cfbfebba6007d
0Gemini0 <tlpelsmaeker@gmail.com>
2017-04-14 12:50:26 +0200

    tweaks to IBM 2

--- Project-1/ibm.py
+++ Project-1/ibm.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import math
+import time
 
 class IBM:
 
@@ -39,12 +40,9 @@ class IBM:
         self.transProbs = trans
         
         
-    def vogel_index(self,i,j,en,fr):
+    def vogel_index(self,i,j,I,J):
         # get the Vogel count index
-        return math.floor(i - (j+1.0) * len(en) / len(fr))
-        
-    
-        
+        return math.floor(i - (j+1.0) * I / J)      
 
     def train_ibm(self, pairs, termination_criteria, threshold, transProbs = False, model = "ibm1"):
         # trains an ibm 1 model
@@ -52,7 +50,7 @@ class IBM:
         converged = False
         logLikelihood = []
         if not transProbs:
-            transProbs = self.transProbs # initialize_ibm_1(transProbs)
+            transProbs = self.transProbs # initialize_ibm(transProbs)
         else:
             self.uniform_init(transProbs)
             transProbs = self.transProbs
@@ -62,14 +60,17 @@ class IBM:
             countsVogel = {}
             probsVogel = {}
             for pair in pairs:
+                I = len(pair[0]) # english sentence length
+                J = len(pair[1]) # french sentence length
                 for i, enWord in enumerate(pair[0]):
                     for j, frWord in enumerate(pair[1]):
-                        countsVogel[self.vogel_index(i,j,pair[0],pair[1])] = 0.0 # check: do we need j + 1 cuz null  
+                        countsVogel[self.vogel_index(i,j,I,J)] = 0.0 # check: do we need j + 1 cuz null  
             length = len(countsVogel.keys())
             for key in countsVogel:
                 probsVogel[key] = 1.0 / length
             
         while (not converged):
+            start = time.time()
             logLike = 0
 
             # set all counts for the translation model to zero
@@ -81,24 +82,24 @@ class IBM:
                 for secKey in transProbs[key]:
                     counts[key][secKey] = 0.0
 
-            # loop over sentences, french words and english words
             # Expectation - step
             print "E"
             for pair in pairs:
-
+                I = len(pair[0]) # english sentence length
+                J = len(pair[1]) # french sentence length
+                
                 #if self.model == self.IBM2:
                 #     logLike += #log of IBM2 alignment probability
 
-
-                logLike += -(len(pair[1]) * np.log(len(pair[0])+1))
+                logLike += -(J * np.log(I+1))
                 for j, fWord in enumerate(pair[1]):
                     # calculate the normalizer of the posterior probability of this french word
                     normalizer = 0.0
                     normalizerVogel = 0.0
                     for i, eWord in enumerate(pair[0]):
                         if self.model == self.IBM2:
-                            normalizer += transProbs[eWord][fWord] * probsVogel[self.vogel_index(i,j,pair[0],pair[1])]
-                            normalizerVogel += probsVogel[self.vogel_index(i,j,pair[0],pair[1])]
+                            normalizer += transProbs[eWord][fWord] * probsVogel[self.vogel_index(i,j,I,J)]
+                            normalizerVogel += probsVogel[self.vogel_index(i,j,I,J)]
                         else:
                             normalizer += transProbs[eWord][fWord]
                             
@@ -107,13 +108,12 @@ class IBM:
                     # get the expected counts based on the posterior probabilities
                     for i, eWord in enumerate(pair[0]):
                         if self.model == self.IBM2:
-                            delta = probsVogel[self.vogel_index(i,j,pair[0],pair[1])] * transProbs[eWord][fWord] / normalizer
-                            countsVogel[self.vogel_index(i,j,pair[0],pair[1])] += delta # do we only need to take the maximum probable likelihood?
+                            delta = probsVogel[self.vogel_index(i,j,I,J)] * transProbs[eWord][fWord] / normalizer
+                            countsVogel[self.vogel_index(i,j,I,J)] += delta # do we only need to take the maximum probable likelihood?
                         else:
                             delta = transProbs[eWord][fWord] / normalizer
                         counts[eWord][fWord] += delta
-                        countsEnglish[eWord] += delta
-                          
+                        countsEnglish[eWord] += delta                
 
             logLikelihood.append(logLike)
             print logLike
@@ -135,5 +135,8 @@ class IBM:
                 # update Vogel-based alignment probabilities
                 normalizer = sum(countsVogel.itervalues())
                 probsVogel = {k: v / normalizer for k, v in countsVogel.iteritems()}
+                
+            end = time.time()
+            print end-start
 
         return transProbs, probsVogel
