b2c8459b616f0c2d726ecfaeab3d5e9133c8cb0c
0Gemini0 <tlpelsmaeker@gmail.com>
2017-04-17 16:43:18 +0200

    ibm class can now load previous model

--- Project-1/ibm.py
+++ Project-1/ibm.py
@@ -2,34 +2,45 @@ import random
 import numpy as np
 import math
 import time
+import cPickle
+
 
 class IBM:
 
     IBM1 = 'ibm1'
     IBM2 = 'ibm2'
-    
 
-    def _init_(self, transProbs, model = "ibm1", method = "uniform", path = ""):
+    def __init__(self, transProbs, vogelProbs=dict, model="ibm1", method="uniform", path="", preloaded=False):
         self.model = model
-        if self.model == IBM1:
-            self.uniform_init(transProbs)
-        elif self.model == IBM2:
-            if method == "uniform":
+        self.method = method
+        self.preloaded = preloaded
+
+        if self.model == self.IBM1:
+            if not preloaded:
                 # uniform parameter initialization
                 self.uniform_init(transProbs)
-            elif method == "random":
-                # do some random init
-                self.random_init
-            elif method == "ibm-1":
-                # load or train ibm-1 as init step
-                if path is not "":
-                    self.transProbs = cPickle.load(open(path, 'rb'))
-                else:
-                    print "provide a path to ibm-1 parameters"
+            else:
+                self.transProbs = transProbs
+        elif self.model == self.IBM2:
+            if not preloaded:
+                if method == "uniform":
+                    # uniform parameter initialization
+                    self.uniform_init(transProbs)
+                elif method == "random":
+                    # do some random init
+                    self.random_init(transProbs)
+                elif method == "ibm-1":
+                    # load or train ibm-1 as init step
+                    if path is not "":
+                        self.transProbs = cPickle.load(open(path, 'rb'))
+                    else:
+                        print "provide a path to ibm-1 parameters"
+            else:
+                self.transProbs = transProbs
+                self.vogelProbs = vogelProbs
         else:
             print "invalid model"
-           
-        
+
     def uniform_init(self, transProbs):
         trans = {}
         for key in transProbs:
@@ -43,43 +54,49 @@ class IBM:
         trans = {}
         for key in transProbs:
             trans[key] = {}
-            vocabSize = len(transProbs[key].keys())
             for secKey in transProbs[key]:
                 trans[key][secKey] = random.random()
             normalizer = sum(trans[key].itervalues())
             trans[key] = {k: v / normalizer for k, v in trans[key].iteritems()}
         self.transProbs = trans
 
-    def vogel_index(self,i,j,I,J):
+    @staticmethod
+    def vogel_index(i, j, I, J):
         # get the Vogel count index
-        return math.floor(i - (j+1.0) * I / J)      
+        return math.floor(i - (j+1.0) * I / J)
 
-    def train_ibm(self, pairs, termination_criteria, threshold, transProbs = False, model = "ibm1"):
+    def train_ibm(self, pairs, termination_criteria, threshold):
         # trains an ibm 1 model
-        self.model = model
         converged = False
         logLikelihood = []
-        if not transProbs:
-            transProbs = self.transProbs # initialize_ibm(transProbs)
-        else:
-            self.uniform_init(transProbs)
-            transProbs = self.transProbs
-            
+
+        transProbs = self.transProbs  # initialize_ibm(transProbs)
+
         if self.model == self.IBM2:
             # initialize vogel count parameter vector
-            countsVogel = {}
-            probsVogel = {}
-            for pair in pairs:
-                I = len(pair[0]) # english sentence length
-                J = len(pair[1]) # french sentence length
-                for i, enWord in enumerate(pair[0]):
-                    for j, frWord in enumerate(pair[1]):
-                        countsVogel[self.vogel_index(i,j,I,J)] = 0.0 # check: do we need j + 1 cuz null  
-            length = len(countsVogel.keys())
-            for key in countsVogel:
-                probsVogel[key] = 1.0 / length
-            
-        while (not converged):
+            if not self.preloaded:
+                countsVogel = {}
+                vogelProbs = {}
+                for pair in pairs:
+                    I = len(pair[0])  # english sentence length
+                    J = len(pair[1])  # french sentence length
+                    for i, enWord in enumerate(pair[0]):
+                        for j, frWord in enumerate(pair[1]):
+                            countsVogel[self.vogel_index(i, j, I, J)] = 0.0  # check: do we need j + 1 cuz null
+                if self.method == "uniform":
+                    length = len(countsVogel.keys())
+                    for key in countsVogel:
+                        vogelProbs[key] = 1.0 / length
+                else:
+                    for key in countsVogel:
+                        vogelProbs[key] = random.random()
+                    normalizer = sum(vogelProbs.itervalues())
+                    vogelProbs = {k: v / normalizer for k, v in vogelProbs.iteritems()}
+            else:
+                vogelProbs = self.vogelProbs
+                countsVogel = {k: 0.0 for k in vogelProbs.keys()}
+
+        while not converged:
             start = time.time()
             logLike = 0
 
@@ -95,11 +112,8 @@ class IBM:
             # Expectation - step
             print "E"
             for pair in pairs:
-                I = len(pair[0]) # english sentence length
-                J = len(pair[1]) # french sentence length
-                
-                #if self.model == self.IBM2:
-                #     logLike += #log of IBM2 alignment probability
+                I = len(pair[0])  # english sentence length
+                J = len(pair[1])  # french sentence length
 
                 logLike += -(J * np.log(I+1))
                 for j, fWord in enumerate(pair[1]):
@@ -108,18 +122,17 @@ class IBM:
                     normalizerVogel = 0.0
                     for i, eWord in enumerate(pair[0]):
                         if self.model == self.IBM2:
-                            normalizer += transProbs[eWord][fWord] * probsVogel[self.vogel_index(i,j,I,J)]
-                            normalizerVogel += probsVogel[self.vogel_index(i,j,I,J)]
+                            normalizer += transProbs[eWord][fWord] * vogelProbs[self.vogel_index(i, j, I, J)]
+                            normalizerVogel += vogelProbs[self.vogel_index(i, j, I, J)]
                         else:
                             normalizer += transProbs[eWord][fWord]
-                            
 
                     logLike += np.log(normalizer)
                     # get the expected counts based on the posterior probabilities
                     for i, eWord in enumerate(pair[0]):
                         if self.model == self.IBM2:
-                            delta = probsVogel[self.vogel_index(i,j,I,J)] * transProbs[eWord][fWord] / normalizer
-                            countsVogel[self.vogel_index(i,j,I,J)] += delta # do we only need to take the maximum probable likelihood?
+                            delta = vogelProbs[self.vogel_index(i, j, I, J)] * transProbs[eWord][fWord] / normalizer
+                            countsVogel[self.vogel_index(i, j, I, J)] += delta  # do we only need to take the maximum probable likelihood?
                         else:
                             delta = transProbs[eWord][fWord] / normalizer
                         counts[eWord][fWord] += delta
@@ -144,14 +157,14 @@ class IBM:
             if self.model == self.IBM2:
                 # update Vogel-based alignment probabilities
                 normalizer = sum(countsVogel.itervalues())
-                probsVogel = {k: v / normalizer for k, v in countsVogel.iteritems()}
+                vogelProbs = {k: v / normalizer for k, v in countsVogel.iteritems()}
                 
             end = time.time()
             print end-start
 
-        return transProbs, probsVogel
+        return transProbs, vogelProbs
 
-    def get_alignments(self, pairs, transProbs, model="ibm1", vogelProbs= ""):
+    def get_alignments(self, pairs, transProbs, model="ibm1", vogelProbs=""):
         """Get the predicted alignments on sentence pairs from a trained ibm model 1 or 2"""
         alignments = []
         for k, pair in enumerate(pairs):
